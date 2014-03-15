8c8a13a76ccda970dc699784e99a6e6bb65c9f18
Luke Baumann <mydoghasissues@gmail.com>
2014-03-14 21:32:04 -0700

    Changed test.sh

--- FinalProject/test.sh
+++ FinalProject/test.sh
@@ -3,16 +3,6 @@
 PROGRAM_NAME="DFT"
 OUTPUT_NAME="test.out"
 
-if [ $# -ne 1 ]
-then
-   echo "usage: $0 commit message"
-   exit $?
-fi
-
-echo $1
-
-
-
 echo "Compiling program..."
 sleep 0.5
 gcc "$PROGRAM_NAME".c -o $PROGRAM_NAME || { echo "Compiling error"; exit $?; }
@@ -21,11 +11,15 @@ echo "Executing program..."
 sleep 0.5
 time $PROGRAM_NAME > $OUTPUT_NAME || { echo "Executing error"; exit $?; }
 
+if [ $# -ne 0 ]
+then
+   echo "Committing changes..."
+   sleep 0.5
+   git add "--all"
+   git commit "-am $1"
+fi
+
 echo "Opening file..."
 sleep 0.5
 less test.out
 
-echo "Committing changes..."
-sleep 0.5
-"git add --all"
-"git commit -am $1"
